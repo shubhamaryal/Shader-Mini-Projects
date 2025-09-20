ff3945c4d577aa01a8d85b70266011dcd6f07bbe
shubhamaryal <aryalshubham62@gmail.com>
2025-09-20 11:20:35 +0545

    coffee smoke

---
+++ 4. Coffee Smoke/src/shaders/coffeeSmoke/fragment.glsl
@@ -0,0 +1,40 @@
+uniform sampler2D uPerlinTexture;
+uniform float uTime;
+
+varying vec2 vUv;
+
+void main() {
+    // Scale and animate 
+    vec2 smokeUv = vUv; 
+    smokeUv.x *= 0.5;
+    smokeUv.y *= 0.3;
+    smokeUv.y -= uTime * 0.03;
+
+    // Smoke
+    // vec4 smoke = texture(uPerlinTexture, vUv);
+    float smoke = texture(uPerlinTexture, smokeUv).r;
+
+    // Remap 
+    smoke = smoothstep(0.4, 1.0, smoke);
+
+    // Edges
+    // smoke = 1.0; 
+    smoke *= smoothstep(0.0, 0.1, vUv.x);
+    smoke *= smoothstep(1.0, 0.9, vUv.x);
+    // // We can multiply the value too for the other side
+    // smoke = (1.0 - (smoothstep(0.9, 1.0, vUv.x)));
+    smoke *= smoothstep(0.0, 0.1, vUv.y);
+    smoke *= smoothstep(1.0, 0.4, vUv.y);
+
+    // Final color
+    // gl_FragColor = vec4(0.0, 1.0, 0.0, 1.0);
+    // gl_FragColor = vec4(vUv, 1.0, 1.0);
+    // gl_FragColor = vec4(vec3(smoke), 1.0);
+    // gl_FragColor = vec4(smoke, smoke, smoke, 1.0);
+    // gl_FragColor = vec4(1.0, 1.0, 1.0, smoke);
+    gl_FragColor = vec4(0.6, 0.3, 0.2, smoke);
+    // gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+
+    #include <tonemapping_fragment>
+    #include <colorspace_fragment>
+}
