45d33a99b843d830bc7f98c4251a0928c0c55c91
shubhamaryal <aryalshubham62@gmail.com>
2025-10-02 19:59:27 +0545

    fireworks

--- 6. Fireworks/src/main.js
+++ 6. Fireworks/src/main.js
@@ -2,12 +2,14 @@ import * as THREE from 'three'
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
 import GUI from 'lil-gui'
 import gsap from 'gsap'
+import { Sky } from 'three/addons/objects/Sky.js'
 
 import fireworkVertexShader from './shaders/firework/vertex.glsl?raw'
 import fireworkFragmentShader from './shaders/firework/fragment.glsl?raw'
 
 // console.log(fireworkfragmentShader)
 // console.log(fireworkvertexShader)
+
 
 /**
  * Base
@@ -186,7 +188,7 @@ const createFirework = (count, position, size, texture, radius, color) => {
 //   1,                                // Radius 
 //   new THREE.Color('#8affff'),     // Color
 // )
-
+ 
 const createRandomFirework = () => {
   const count = Math.round(400 + Math.random() * 1000)
   const position = new THREE.Vector3(
@@ -224,6 +226,65 @@ window.addEventListener('click', createRandomFirework)
 //   new THREE.MeshBasicMaterial()
 // )
 // scene.add(test)
+
+/**
+ * Sky
+ */
+// Add sky   
+// sky = new Sky();
+const sky = new Sky();
+sky.scale.setScalar( 450000 );
+scene.add( sky );
+
+// sun = new THREE.Vector3();
+const sun = new THREE.Vector3();
+
+/// GUI
+
+const effectController = {
+  turbidity: 10,
+  rayleigh: 3,
+  mieCoefficient: 0.005,
+  // mieDirectionalG: 0.7,
+  mieDirectionalG: 0.95,
+  // elevation: 2,
+  elevation: -2.2,
+  azimuth: 180,
+  exposure: renderer.toneMappingExposure
+};
+
+function guiChanged() {
+
+  const uniforms = sky.material.uniforms;
+  uniforms[ 'turbidity' ].value = effectController.turbidity;
+  uniforms[ 'rayleigh' ].value = effectController.rayleigh;
+  uniforms[ 'mieCoefficient' ].value = effectController.mieCoefficient;
+  uniforms[ 'mieDirectionalG' ].value = effectController.mieDirectionalG;
+
+  const phi = THREE.MathUtils.degToRad( 90 - effectController.elevation );
+  const theta = THREE.MathUtils.degToRad( effectController.azimuth );
+
+  sun.setFromSphericalCoords( 1, phi, theta );
+
+  uniforms[ 'sunPosition' ].value.copy( sun );
+
+  renderer.toneMappingExposure = effectController.exposure;
+  renderer.render( scene, camera );
+
+}
+
+// const gui = new GUI();
+
+gui.add( effectController, 'turbidity', 0.0, 20.0, 0.1 ).onChange( guiChanged );
+gui.add( effectController, 'rayleigh', 0.0, 4, 0.001 ).onChange( guiChanged );
+gui.add( effectController, 'mieCoefficient', 0.0, 0.1, 0.001 ).onChange( guiChanged );
+gui.add( effectController, 'mieDirectionalG', 0.0, 1, 0.001 ).onChange( guiChanged );
+// gui.add( effectController, 'elevation', 0, 90, 0.1 ).onChange( guiChanged );
+gui.add( effectController, 'elevation', -3, 10, 0.01 ).onChange( guiChanged );
+gui.add( effectController, 'azimuth', - 180, 180, 0.1 ).onChange( guiChanged );
+gui.add( effectController, 'exposure', 0, 1, 0.0001 ).onChange( guiChanged );
+
+guiChanged();
 
 /**
  * Animate
