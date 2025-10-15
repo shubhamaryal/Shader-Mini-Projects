330d2da654e8644cec829c578269efa8089bf0ff
shubhamaryal <aryalshubham62@gmail.com>
2025-10-15 23:08:26 +0545

    lights shading

--- 7. Lights Shading/src/shaders/shading/fragment.glsl
+++ 7. Lights Shading/src/shaders/shading/fragment.glsl
@@ -10,7 +10,14 @@ varying vec3 vNormal;
 
 vec3 directionalLight(vec3 lightColor, float lightIntensity, vec3 normal, vec3 lightPosition) {
     vec3 lightDirection = normalize(lightPosition);
-    return lightColor * lightIntensity;
+
+    // Shading 
+    float shading = dot(normal, lightDirection);
+    shading = max(0.0, shading);
+
+    // return lightColor * lightIntensity;
+    // return vec3(shading);
+    return lightColor * lightIntensity * shading;
 }
 
 void main() {
@@ -21,10 +28,10 @@ void main() {
     // light += ambientLight();
     // light += ambientLight(vec3(1.0), 0.2);
     // light += ambientLight(vec3(1.0, 0.0, 0.0), 0.2);
-    // light += ambientLight(
-    //     vec3(1.0),
-    //     0.03
-    // );
+    light += ambientLight(
+        vec3(1.0),
+        0.03
+    );
     // light += directionalLight(
     //     vec3(0.1, 0.1, 1.0),
     //     1.0
--- 7. Lights Shading/src/shaders/shading/vertex.glsl
+++ 7. Lights Shading/src/shaders/shading/vertex.glsl
@@ -5,6 +5,10 @@ void main() {
     vec4 modelPosition = modelMatrix * vec4(position, 1.0);
     gl_Position = projectionMatrix * viewMatrix * modelPosition;
 
+    // Model normal
+    vec4 modelNormal = modelMatrix * vec4(normal, 0.0);
+
     // Varying
-    vNormal = normal; 
+    // vNormal = normal; 
+    vNormal = modelNormal.xyz;
 }
