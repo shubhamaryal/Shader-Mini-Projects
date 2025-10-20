dc334cc39fd3f4ae58208e96e9f320d71e90d744
shubhamaryal <aryalshubham62@gmail.com>
2025-10-20 22:33:12 +0545

    light shading

--- 7. Lights Shading/src/shaders/shading/fragment.glsl
+++ 7. Lights Shading/src/shaders/shading/fragment.glsl
@@ -9,7 +9,7 @@ varying vec3 vPosition;
 // }
 #include "../includes/ambientLight.glsl"
 
-vec3 directionalLight(vec3 lightColor, float lightIntensity, vec3 normal, vec3 lightPosition, vec3 viewDirection) {
+vec3 directionalLight(vec3 lightColor, float lightIntensity, vec3 normal, vec3 lightPosition, vec3 viewDirection, float specularPower) {
     vec3 lightDirection = normalize(lightPosition);
     // vec3 lightReflection = reflect(lightDirection, normal);
     vec3 lightReflection = reflect(- lightDirection, normal);
@@ -20,15 +20,18 @@ vec3 directionalLight(vec3 lightColor, float lightIntensity, vec3 normal, vec3 l
 
     // Specular 
     // float specular = dot(lightReflection, lightDirection);
-    float specular = - dot(lightReflection, lightDirection);
+    float specular = - dot(lightReflection, viewDirection);
     specular = max(0.0, specular);
-    specular = pow(specular, 20.0);
+    // specular = pow(specular, 20.0);
     // specular = pow(specular, 19.0);
+    // specular = pow(specular, 20.0);
+    specular = pow(specular, specularPower);
 
     // return lightColor * lightIntensity;
     // return vec3(shading);
     // return lightColor * lightIntensity * shading;
-    return vec3(specular);
+    // return vec3(specular);
+    return lightColor * lightIntensity * shading + specular;
 }
 
 void main() {
@@ -63,12 +66,20 @@ void main() {
     //     vec3(0.0, 0.0, 3.0),
     //     viewDirection
     // );
+    // light += directionalLight(
+    //     vec3(0.1, 0.1, 1.0),
+    //     1.0,
+    //     normal,
+    //     vec3(0.0, 0.0, 3.0),
+    //     viewDirection
+    // );
     light += directionalLight(
         vec3(0.1, 0.1, 1.0),
         1.0,
         normal,
         vec3(0.0, 0.0, 3.0),
-        viewDirection
+        viewDirection,
+        20.0
     );
     color *= light;
 
